d5220457f3269c4e92b9f5a386fefd0c52d78956
Carambole <131371657+carambole25@users.noreply.github.com>
2025-03-01 15:58:56 +0100

    Update installeur.ps1

--- agent/windows/installeur.ps1
+++ agent/windows/installeur.ps1
@@ -3,5 +3,8 @@ mkdir save
 $ip_indexer = Read-Host 'Ip of the indexer'
 (Get-Content -Path 'main.py') -replace 'IP_INDEXER', $ip_indexer | Set-Content -Path main.py
 
-$absolute_path_of_main = Read-Host 'Absolute path of main.py'
-(Get-Content -Path 'launcher.ps1') -replace 'PATH_MAIN', $absolute_path_of_main | Set-Content -Path launcher.ps1
+pip install pyinstaller
+pip install evtx
+pyinstaller --onefile --noconsole main.py
+
+echo "you need to create a task with task scheduler with maximum privileges to start the dist/main.exe when starting a session"
