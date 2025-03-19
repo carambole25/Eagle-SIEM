0d56604f2d30a3e5e00b5b17b46351e75755fe9f
carambole <carambole258@gmail.com>
2025-03-19 19:13:30 +0100

    NIDS : detection de DNS tunneling

--- agent/linux/nids_func.py
+++ agent/linux/nids_func.py
@@ -1,9 +1,25 @@
 import threading
 import time
-from scapy.all import sniff
+import datetime
+from scapy.all import sniff, DNS, DNSQR
 
 packets_list = []
 
+LOG_PATH = "eagle_nids.log"
+
+date = str(datetime.datetime.now())
+
+# --------- FONCTION DE DETECTION
+def test_dns(p):
+
+    # DNS tunneling
+    domain_name = p[DNSQR].qname.decode()
+    if len(domain_name) > 30:
+        write_alert(f"A domain name exceeds 30 characters. This could be DNS tunneling. {domain_name}\n")
+
+# ---------
+
+# --------- CORE
 def sniffing():
     global packets_list
     while True:
@@ -15,9 +31,21 @@ def analyse():
     global packets_list
     while True:
         for p in packets_list:
-            print(f"Analyse du paquet: {p.summary()}")
-            packets_list.pop(0)
+            if p.haslayer(DNS):
+                test_dns(p)
+                packets_list.pop(0)
+
+
+            else:
+                print(f"{p.summary()}")
+                packets_list.pop(0)
         time.sleep(1)
+
+def write_alert(alert):
+    global date
+    alert = date + " : " + alert
+    print(alert)
+    open(LOG_PATH, "a+").write(alert)
 
 def main():
     recuperation_packets = threading.Thread(target=sniffing, daemon=True)
